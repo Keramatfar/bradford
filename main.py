fa375c94d78923ffc78fafc032080dac12cb54dc
Keramatfar <keramatfar.a.s@gmail.com>
2020-11-14 10:32:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+data = pd.read_excel('all.xlsx', header = None)
+
 p = data[0].to_numpy()
 loss = 10**8
 i1, i2 = 0, 0
@@ -12,4 +16,13 @@ for c1 in range(len(data)-1):
             loss = loss_temp   
             i1, i2 = c1, c2
             s1, s2, s3 = p1, p2, p3
-print('Zone 1:', i1, 'Zone 2:', i2-i1, 'Zone 3:', len(data) - i2) #These are the points which you break your list at them.
+indices = ['Zone 1', 'Zone 2', 'Zone 3', 'Total']
+cj = [i1, i2-i1, len(data) - i2]
+cj.append(np.sum(cj))
+ss = [s1, s2, s3]
+ss.append(np.sum(ss))
+mps = ['-', (i2-i1)/i1, (len(data) - i2)/(i2-i1)]
+mps.append(np.mean(mps[1:]))
+output = pd.DataFrame({'Number of Journals': cj, 
+                       'Number of Articles': ss, 
+                       'Multiplier': mps}, index = indices)
